1aed33e60a9e5722eff2bc79b11c7d3d16d82ccd
mual <pramualkr@yahoo.com>
2017-12-17 09:39:42 +0700

    Wall paper ok

--- app/src/main/java/com/example/pramual/firstandroidapp/MainActivity.java
+++ app/src/main/java/com/example/pramual/firstandroidapp/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.pramual.firstandroidapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.pramual.firstandroidapp.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        // Add Frgment to Activity
+
+
+
+        if (savedInstanceState== null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragment, new MainFragment())
+                    .commit();
+        }
+    } // Main Method
+}  // main Class
